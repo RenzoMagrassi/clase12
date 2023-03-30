1ffa0a4257ec1ea0a4a24a405ee764d66d5ed131
RenzoMagrassi <renzo_m7@hotmail.com>
2023-03-29 22:58:04 -0300

    trabajo fin

--- martin.js
+++ martin.js
@@ -1,8 +1,5 @@
 console.log("Hola mundo");
-<<<<<<< HEAD
 console.log("Hola Argentina");
 console.log("Adios");
-=======
 console.log("Nueva version del Hola Mundo");
 console.log("Muchachos, ahora nos volvimos a ilusionar");
->>>>>>> 03852c297979e421f3492e263f8dfb6d12050109
